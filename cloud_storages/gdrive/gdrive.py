d88339548b3a143ac3f3bce2de45222e45e89077
toplenboren <toplenboren@gmail.com>
2021-11-04 23:55:54 +0500

    finish google drive auth method

--- cloud_storages/gdrive/gdrive.py
+++ cloud_storages/gdrive/gdrive.py
@@ -1,18 +1,18 @@
 from __future__ import print_function
 
+import json
 from typing import List
 
 from cloud_storages.http_shortcuts import *
 from database.database import Database
 from models.models import StorageMetaInfo, Resource, Size
 from cloud_storages.storage import Storage
+from client_config import GOOGLE_DRIVE_CONFIG
 
-import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-
 
 GOOGLE_DRIVE = 'gdrive'
 
@@ -25,24 +25,17 @@ class GDriveStorage(Storage):
     @classmethod
     # todo (toplenboren) remove databaase argument dependency :(
     def auth(cls, db: Database):
-        """
-        Shows basic usage of the Drive v3 API.
-        Prints the names and ids of the first 10 files the user has access to.
-        """
         creds = None
         creds_from_db = db.get(GOOGLE_DRIVE)
         if creds_from_db:
-            creds = Credentials.from_authorized_user_info(creds_from_db, SCOPES)
-        # If there are no (valid) credentials available, let the user log in.
+            creds = Credentials.from_authorized_user_info(json.loads(creds_from_db), SCOPES)
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
             else:
-                flow = InstalledAppFlow.from_client_secrets_file('./credentials.json', SCOPES)
+                flow = InstalledAppFlow.from_client_config(GOOGLE_DRIVE_CONFIG, SCOPES)
                 creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open('token.json', 'w') as token:
-                token.write(creds.to_json())
+            db.set(GOOGLE_DRIVE, creds.to_json())
 
     @classmethod
     def _deserialize_resource(cls, json: dict) -> Resource or None:
@@ -91,18 +84,7 @@ class GDriveStorage(Storage):
                              f"{str(response.status_code)} — {response.json()['message']}")
 
     def get_meta_info(self) -> StorageMetaInfo:
-        """
-        Gets meta info of storage
-        """
-        response = get_with_OAuth('https://cloud-api.yandex.net/v1/disk/', token=self.token)
-        if response.status_code == 200:
-            response_read = response.json()
-            used_space = response_read['used_space']
-            total_space = response_read['total_space']
-            return StorageMetaInfo(int(used_space), int(total_space))
-        else:
-            raise ValueError(f"Something went wrong with YD: Response: "
-                             f"{str(response.status_code)} — {response.json()['message']}")
+
 
     def create_path(self, remote_path: List[str]) -> None:
         """
@@ -187,29 +169,38 @@ class GDriveStorage(Storage):
 
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+SCOPES = [
+    'https://www.googleapis.com/auth/drive.metadata.readonly'
+]
 
 
 def main():
     storage = GDriveStorage(None)
-    database = Database('../../storage.db')
-    storage.auth(database)
+    db = Database('../storage.db')
+    storage.auth(db)
 
-    creds = {}
+    authed_storage = GDriveStorage(json.loads(db.get(GOOGLE_DRIVE))['token'])
+    meta = authed_storage.get_meta_info()
 
-    service = build('drive', 'v3', credentials=creds)
+    print(meta)
 
-    # Call the Drive v3 API
-    results = service.files().list(
-        pageSize=10, fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
-
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
+    #
+    # creds = json.loads(db.get(GOOGLE_DRIVE))
+    #
+    # service = build('drive', 'v3', credentials=creds)
+    #
+    # # Call the Drive v3 API
+    # results = service.files().list(
+    #     pageSize=10, fields="nextPageToken, files(id, name)"
+    # ).execute()
+    # items = results.get('files', [])
+    #
+    # if not items:
+    #     print('No files found.')
+    # else:
+    #     print('Files:')
+    #     for item in items:
+    #         print(u'{0} ({1})'.format(item['name'], item['id']))
 
 
 if __name__ == '__main__':
